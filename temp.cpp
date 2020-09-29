9ebc701ad8c51eff75e0df4f0614b80ec7ba4bd6
Kaustuv K Chattopadhyay <56028031+Kaustuv942@users.noreply.github.com>
2020-09-29 12:18:04 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,25 +1,12 @@
-//kkc
 #include <iostream>
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
-
-void solve(){
-	ll n;
-	cin>>n;
-	cout<<n<<endl;
-    
+void solve(){    
+	//write your code here
 }
 
 int main(){
-    fastio
-    int t=1;
-    cin>>t;
-    // freopen("input.txt", "r", stdin); 
-    // freopen("output.txt", "w", stdout); 
-    while(t--){  
-        solve();
-    }
-    // cerr << "Time elapsed: " << clock() / (double) CLOCKS_PER_SEC << endl;
+    int t=1;cin>>t;    
+    while(t--){ solve();}    
     return 0;
 }
